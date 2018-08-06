7e7c306afb2eaadffcb862a2c75013818805c126
kicker <kicker@GARAM-DELL>
2018-08-06 18:09:45 +0900

    * 배열의 초기값 설정 및 배열의 값을 가져오는 방법

--- src/garam/calendar/Calendar.java
+++ src/garam/calendar/Calendar.java
@@ -1,5 +1,5 @@
 package garam.calendar;
-
+import java.util.Scanner ;
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -12,6 +12,38 @@ public class Calendar {
 		System.out.println(" 22 23 24 25 26 27 28");
 		System.out.println(" 29 30 31            ");
 
+		
+		// 슛자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+        int[] maxDayOfMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 } ; 
+		
+        // maxDayOfMonth[0] = 31;
+        // maxDayOfMonth[1] = 28;
+        // maxDayOfMonth[2] = 31;
+        // maxDayOfMonth[3] = 30;
+        // maxDayOfMonth[4] = 31;
+        // maxDayOfMonth[5] = 30;
+        // maxDayOfMonth[6] = 31;
+        // maxDayOfMonth[7] = 31;
+        // maxDayOfMonth[8] = 30;
+        // maxDayOfMonth[9] = 31;
+        // maxDayOfMonth[10]= 30;
+        // maxDayOfMonth[11] = 31;
+        
+        
+		
+        System.out.print("월을 입력하세요 : ");
+		Scanner scanner = new Scanner(System.in) ;
+        
+		int inValMonth = Integer.parseInt(scanner.nextLine()) ;
+		
+		if ( (inValMonth > 0 ) && (inValMonth < 13) ) {
+		  System.out.println("입력하신 " + inValMonth + "월의 마지막 날은 " + maxDayOfMonth[inValMonth-1] + " 입니다.");
+		} else {
+			System.out.println("잘못된 월을 입력하셨습니다. " ) ;
+		}
+			
+		scanner.close();
+		
 	}
 
 }
