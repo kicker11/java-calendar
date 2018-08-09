92624ca8bc6325a46c291cbd33efff3278f3b0fa
kicker <kicker@GARAM-DELL>
2018-08-09 10:32:31 +0900

    * class 분리 및 년도에 따른 윤년계산에 따른 2월의 날자 구하는 로직 추가

--- src/garam/calendar/Calendar.java
+++ src/garam/calendar/Calendar.java
@@ -1,12 +1,25 @@
-package garam.calendar;
-import java.util.Scanner ;
+package garam.calendar;import java.util.Scanner ;
 public class Calendar {
 	
 	public static final int[] maxDayOfMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 } ;
+	public static final int[] maxDayOfMonthLeap = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 } ;
 	public int[][] calMon = new int[12][31];  
+	
+	public boolean isLeapYear(int year){
+		if ( year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0)) {
+			return true ;
+		} else {		
+		    return false;
+		}
+	}
 
-	public int getMaxDaysOfMonth(int mon) {
-		return maxDayOfMonth[mon-1] ;
+	public int getMaxDaysOfMonth(int year, int mon) {
+		
+		if (isLeapYear(year)) {
+			return maxDayOfMonthLeap[mon-1] ;
+		} else {
+			return maxDayOfMonth[mon-1] ;
+		}
 	}
 	
 
@@ -18,14 +31,14 @@ public class Calendar {
         }
 	}
 	
-	public void calPrint(int mon) {
+	public void calPrint(int year, int mon) {
 		System.out.println("   일   월   화   수   목   금   토");
 		System.out.println("-----------------------");
 
         
 		int rep = 0;
         		
-		for ( int currntDay = 0; currntDay < getMaxDaysOfMonth(mon) ; currntDay++ ) {
+		for ( int currntDay = 0; currntDay < getMaxDaysOfMonth(year, mon) ; currntDay++ ) {
 			
 			if ( rep < 6 ) {
 				System.out.printf("%3d" , calMon[mon-1][currntDay] );
@@ -40,62 +53,5 @@ public class Calendar {
 		System.out.print("\n");
 		System.out.print("\n");
     }
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("   일   월   화   수   목   금   토");
-		System.out.println("-----------------------");
-		System.out.println("  1  2  3  4  5  6  7");
-		System.out.println("  8  9 10 11 12 13 14");
-		System.out.println(" 15 16 17 18 19 20 21");
-		System.out.println(" 22 23 24 25 26 27 28");
-		System.out.println(" 29 30 31            ");
-
-		Calendar cal = new Calendar() ;
-		
-		// 슛자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-
-		cal.setInit();
-        
-        String PROMPT = " Cal > " ;
-		Scanner scanner = new Scanner(System.in) ;
-		
-		
-		//System.out.print("반복 횟수 ? : ");
-		//int cnt = scanner.nextInt(); 
-		
-		
-	    int cnt = 0 ;
-	    
-	    while (true) {    
-			//int inValMonth = Integer.parseInt(scanner.nextLine()) ;
-			
-	    	System.out.print(PROMPT);
-		    System.out.print("월을 입력하세요 : "); 
-		    //System.out.println("\n");
-				    	
-	    	int inValMonth = scanner.nextInt() ;
-			
-			if ( (inValMonth > 0 ) && (inValMonth < 13) ) {
-			    System.out.print(PROMPT);
-				System.out.printf("입력하신  %d 월의 달력 입니다.\n" , inValMonth) ;
-				
-				//, cal.getMaxDaysOfMonth(inValMonth));
-				
-				cal.calPrint(inValMonth);
-				
-				cnt++ ;
-			} else {
-				System.out.print(PROMPT);
-				System.out.println("잘못된 월을 입력하셨습니다. " ) ;
-				break ;
-			}
-	    }	
-	    System.out.print(PROMPT);
-	    System.out.println(cnt + "회 만큼 반복하였습니다. Bye~") ;
-		scanner.close();
-		
-	}
 
 }
