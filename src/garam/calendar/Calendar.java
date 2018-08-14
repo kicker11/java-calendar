75ac032b5443695306f61c7f0fd2fb5353bd9273
kicker <kicker@GARAM-DELL>
2018-08-14 18:18:46 +0900

    * HashMap 을 이용한 등록, 조회 구현

--- src/garam/calendar/Calendar.java
+++ src/garam/calendar/Calendar.java
@@ -1,12 +1,42 @@
 package garam.calendar;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Calendar {
 
-	public static final int[] maxDayOfMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	public static final int[] maxDayOfMonthLeap = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	public static final int[] maxDayOfMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	public static final int[] maxDayOfMonthLeap = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	public String[] calMon = new String[37];
 	public int idxOfDays = 0;
+	
+	private HashMap <Date, String> planMap ;
+	
+	public Calendar() {
+		planMap = new HashMap<Date, String>();
+	}
+	
 
+	/**
+	 * 
+	 * @param date ex: "2018-08-14"
+	 * @param plan ex:
+	 * @throws ParseException 
+	 */
+	public void registerPlan(String strDate, String plan) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		System.out.println(date);
+		planMap.put(date, plan) ;
+	}
+	
+	public String searchPlan(String strDate) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		String plan = planMap.get(date);
+		return plan;
+	}
+		
+	
 	public void setSpace(int idx) {
 
 		if (idx != 0) {
@@ -27,52 +57,22 @@ public class Calendar {
 	public int getMaxDaysOfMonth(int year, int mon) {
 
 		if (isLeapYear(year)) {
-			return maxDayOfMonthLeap[mon - 1];
+			return maxDayOfMonthLeap[mon];
 		} else {
-			return maxDayOfMonth[mon - 1];
+			return maxDayOfMonth[mon];
 		}
+		
 	}
 
-	public void setInitVal(int year, int month, String day) {
-
-		switch (day) {
-		case "MO":
-			setSpace(2);
-			idxOfDays = 2;
-			break;
-		case "TU":
-			setSpace(3);
-			idxOfDays = 3;
-			break;
-		case "WE":
-			setSpace(4);
-			idxOfDays = 4;
-			break;
-		case "TH":
-			setSpace(5);
-			idxOfDays = 5;
-			break;
-		case "FR":
-			setSpace(6);
-			idxOfDays = 6;
-			break;
-		case "ST":
-			setSpace(7);
-			idxOfDays = 7;
-			break;
-		default:
-			setSpace(1);
-			idxOfDays = 1;
-			break;
-		}
+	public void setInitVal(int year, int month) {
 
 		int maxArrayLength = (getMaxDaysOfMonth(year, month) + idxOfDays);
 		for (int j = idxOfDays; j < maxArrayLength; j++) {
-			calMon[j] = String.valueOf((j + 1) - idxOfDays);
+			calMon[j] = String.valueOf((j) - idxOfDays);
 		}
 	}
 
-	public void calPrint(int year, int mon, String inValDay) {
+	public void calPrint(int year, int mon) {
 		System.out.println("   일   월   화   수   목   금   토");
 		System.out.println("-----------------------");
 
@@ -90,4 +90,43 @@ public class Calendar {
 		System.out.print("\n");
 	}
 
+	
+	private int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 4;  // 1970/JAN/1ST = Thursday
+		
+		int count = 0;
+		
+		for ( int i=syear; i < year ; i++) {
+			int delta = isLeapYear(i) ? 366 : 365 ;
+			count += day -1 ;
+		}
+		
+		for ( int i = 1; i < month ; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta ;
+		}
+		
+		count += day -1 ;
+		
+		int weekday  = ( count + STANDARD_WEEKDAY ) % 7 ;
+		return weekday ;
+		
+	}
+	
+	
+	// Simple Test Code
+	public static void main(String[] args) throws ParseException {
+		Calendar cal = new Calendar();
+		System.out.println(cal.getWeekDay(1970, 1, 1) == 4);
+		System.out.println(cal.getWeekDay(1971, 1, 1) == 5);
+		System.out.println(cal.getWeekDay(1972, 1, 1) == 6);
+		System.out.println(cal.getWeekDay(1973, 1, 1) == 1);
+		System.out.println(cal.getWeekDay(1974, 1, 1) == 2);
+		
+		cal.registerPlan("2018-08-14", "1st 일정 등록");
+		System.out.println(cal.searchPlan("2018-08-14").equals("1st 일정 등록"));
+	}
+	
+	
 }
--- src/garam/calendar/Prompt.java
+++ src/garam/calendar/Prompt.java
@@ -1,54 +1,123 @@
 package garam.calendar;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
+	
+	public static final String PROMPT = "Cal > " ;
+	
+	public void printMenu() {
+		System.out.println("+==========================================+");
+		System.out.println("+ 1. 일정 등록                                                           +");
+		System.out.println("+ 2. 일정 조회                                                          +");
+		System.out.println("+ 3. 달력 표시                                                           +");
+		System.out.println("+ h. 메뉴 출력                                                          +");
+		System.out.println("+ q. 종     료                                                           +");
+		System.out.println("+==========================================+");
+	}
 
-	public void runPrompt() {
+	public void cmdCal(Scanner scanner, Calendar cal) {
 
-		Calendar cal = new Calendar();
+		int cnt = 0;
+		int month = 1;
+		int year = 2017 ;
+		
+		//Scanner scanner = new Scanner(System.in) ;
+		
+		//Calendar cal = new Calendar();
+		
+		System.out.print("YEAR > ");
+		System.out.print("년도를 입력하세요 : ");
+		int inValYear = scanner.nextInt();
+
+		System.out.print("MONTH > ");
+		System.out.print("월을 입력하세요 : ");
+		int inValMonth = scanner.nextInt();
+
+
+		cal.setInitVal(inValYear, inValMonth);
+
+		if ((inValMonth > 0) && (inValMonth < 13)) {
+			System.out.print(PROMPT);
+			System.out.printf("입력하신 %d년  %d 월의 달력 입니다.\n", inValYear, inValMonth);
+
+			cal.calPrint(inValYear, inValMonth);
+
+			cnt++;
+		} else {
+			System.out.print(PROMPT);
+			System.out.println("잘못된 월을 입력하셨습니다. ");
+			return;
+		}
+		System.out.print(PROMPT);
+		//System.out.println(cnt + "회 만큼 반복하였습니다. Bye~");
+		//scanner.close();
+	}
+
+	public void cmdRegister(Scanner scanner, Calendar cal)  {
+		System.out.println("[새 일정 등록]");
+		System.out.println("[날자를 입력해 주세요 ( yyyy-mm-dd )]");
+		String date = scanner.next();
+		System.out.println("[일정을 입력해 주세요 (문장의 끝에 ; 으로 종료하세요]");
+		String text = "";
+		while (true) {
+			String word = scanner.next();
+			text += word + " " ;
+			if (word.endsWith(";")) {
+				break ;
+			}
+		}
+		
+  	   try {
+		cal.registerPlan(date, text);
+	} catch (ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+  	   //scanner.close();
+	}
+	
+	public void cmdSearch(Scanner scanner, Calendar cal) {
+		System.out.println("[일정 검색]");
+		System.out.println("[날자를 입력해 주세요 ( yyyy-mm-dd )] \n");
+		String date = scanner.next();
+		try {
+			String plan = cal.searchPlan(date) ;
+			System.out.println(plan);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.err.println("일정 검색 중 오류가 발생했습니다. \n");
+		}
+		//scanner.close();
+	}
+	
+	public void runPrompt()  {
+		
+		printMenu() ;
 
 		// 슛자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 
 		String PROMPT = " Cal > ";
-		Scanner scanner = new Scanner(System.in);
 
-		int cnt = 0;
-
+		Scanner scanner = new Scanner(System.in) ;
+		
 		while (true) {
-
-			System.out.print("YEAR > ");
-			System.out.print("년도를 입력하세요 : ");
-			int inValYear = scanner.nextInt();
-
-			System.out.print("MONTH > ");
-			System.out.print("월을 입력하세요 : ");
-			int inValMonth = scanner.nextInt();
-
-			System.out.print("DAY > SU MO TU WE TH FR SA \n");
-			System.out.print("1일의 요일을 입력하세요 : ");
-			String inValDay = scanner.next();
-			System.out.print("\n");
-
-			cal.setInitVal(inValYear, inValMonth, inValDay);
-
-			if ((inValMonth > 0) && (inValMonth < 13)) {
-				System.out.print(PROMPT);
-				System.out.printf("입력하신 %d년  %d 월의 달력 입니다.\n", inValYear, inValMonth);
-
-				cal.calPrint(inValYear, inValMonth, inValDay);
-
-				cnt++;
-			} else {
-				System.out.print(PROMPT);
-				System.out.println("잘못된 월을 입력하셨습니다. ");
-				break;
-			}
+			Calendar cal = new Calendar();
+			while (true) {
+				String cmd = scanner.next();
+				if ( cmd.equals("1")) cmdRegister(scanner, cal);
+				else if (cmd.equals("2")) cmdSearch(scanner, cal) ;
+				else if (cmd.startsWith("3")) cmdCal(scanner, cal) ;
+				else if (cmd.startsWith("h")) printMenu() ;
+				else if (cmd.startsWith("q"))  break; 
+			}	
+			System.out.print(PROMPT);
+			System.out.println("Bye~");
 		}
-		System.out.print(PROMPT);
-		System.out.println(cnt + "회 만큼 반복하였습니다. Bye~");
-		scanner.close();
 	}
+		
 
 	public static void main(String[] args) {
 
